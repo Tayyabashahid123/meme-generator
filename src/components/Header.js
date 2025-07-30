e9eab2bfafd31f0a0159a177e57c6cabf98fd497
Tayyaba Shahid <tayyabashahid019@gmail.com>
2025-07-31 03:03:58 +0500

    commit

---
+++ src/components/Header.js
@@ -0,0 +1,13 @@
+import trollFace from "../trollface.png"
+import "../index.css"
+
+export default function Header() {
+    return (
+        <header className="header">
+            <img 
+                src={trollFace} 
+            />
+            <h1>Meme Generator</h1>
+        </header>
+    )
+}
